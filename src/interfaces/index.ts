cc815a1fbb86db8df0ff79df1cfb1aa0e2a45ac7
ayazhafiz <ayaz.hafiz.1@gmail.com>
2019-04-01 19:35:10 -0500

    test: remove GMaps interface

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,31 +1,4 @@
 /**
- * Describes a Google Maps client
- *
- * @interface
- */
-export interface GoogleMapsClient {
-  directions: Function;
-  distanceMatrix: Function;
-  elevation: Function;
-  elevationAlongPath: Function;
-  geocode: Function;
-  geolocate: Function;
-  reverseGeocode: Function;
-  places: Function;
-  placesNearby: Function;
-  placesRadar: Function;
-  place: Function;
-  placesPhoto: Function;
-  placesAutoComplete: Function;
-  placesQueryAutoComplete: Function;
-  snapToRoads: Function;
-  nearestRoads: Function;
-  speedLimits: Function;
-  snappedSpeedLimits: Function;
-  timezone: Function;
-}
-
-/**
  * Describes a CenterOptions Object
  *
  * @interface
@@ -36,49 +9,4 @@ export interface CenterOptions {
   bounds?: number;
   startIndex?: number;
   degree?: number;
-}
-
-/**
- * Describes a DistanceOptions Object
- *
- * @interface
- */
-export interface DistanceOptions {
-  mode?: string;
-  language?: string;
-  avoid?: Array<string>;
-  units?: string;
-  departure_time?: Date | number;
-  arrival_time?: Date | number;
-  transit_mode?: Array<string>;
-  transit_routing_preference?: string;
-  traffic_model?: string;
-}
-
-/**
- * Describes a PlacesOptions Object
- *
- * @interface
- */
-export interface PlacesOptions {
-  language?: string;
-  radius?: number;
-  keyword?: string;
-  minprice?: number;
-  maxprice?: number;
-  name?: string;
-  opennow?: boolean;
-  rankby?: string;
-  type?: string;
-  pagetoken?: string;
-}
-
-/**
- * Describes a TimeZoneOptions Object
- *
- * @interface
- */
-export interface TimeZoneOptions {
-  timestamp?: Date | number;
-  language?: string;
 }
